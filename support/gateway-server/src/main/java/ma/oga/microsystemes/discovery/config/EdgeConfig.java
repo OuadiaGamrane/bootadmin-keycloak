9264c8d0afc06a5ef4925de552ba5a858912f828
oga <ouadia.g@gmail.com>
2018-04-22 13:14:53 +0100

    adding several changes

--- support/gateway-server/src/main/java/ma/oga/microsystemes/discovery/config/EdgeConfig.java
+++ support/gateway-server/src/main/java/ma/oga/microsystemes/discovery/config/EdgeConfig.java
@@ -1,16 +1,10 @@
 package ma.oga.microsystemes.discovery.config;
 
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EdgeConfig {
 
-    @Bean
-    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient) {
-
-        return new DiscoveryClientRouteDefinitionLocator(discoveryClient);
-    }
 }
